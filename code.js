fc73017788516885572b2f50bb77d9dc7e7461aa
Hitoki <46871497+Hitokii@users.noreply.github.com>
2022-09-10 20:30:34 +0200

    Create code.js

---
+++ code.js
@@ -0,0 +1,58 @@
+/*
+"Lumière" par Hitoki
+Commentaire :
+Pour que le programme marche il faut 2 commandes blocks avec ces commandes :
+execute as @e[type="{INSERET NOM}"] run msg {NOM DU ROBOT} on
+execute as @e[type="{INSERET NOM}"] run msg {NOM DU ROBOT} off
+
+le tout relié au levier et voilà ! :D
+
+*/
+
+
+// SERVER INFO
+SERVERIP = "25.67.250.155" // IP du serv
+SERVERPORT = 65428 // PORT du serv
+BOTNAME = "Robot" // nom du robot
+
+const GoveeLED = require('node-govee-led'); // lib API Govee
+const mineflayer = require('mineflayer'); // lib Mineflayer du Robot
+
+var Light = new GoveeLED({
+    apiKey: "{CLE API}", 
+    mac: "{ADRESSE MAC DE LA LED}",
+    model: "{MODEL}"
+})
+
+const client = new mineflayer.createBot({  // Création du Robot
+    username: BOTNAME,
+    host: SERVERIP,
+    port: SERVERPORT,
+    version: 1.17
+})
+
+
+
+console.log("\033[1;34mTrying to access " + SERVERIP+":"+SERVERPORT + " as : " + BOTNAME)
+
+client.on('whisper', (username,message) => {   // Interaction   ON/OFF
+    if (message == "on") {
+        Light.turnOn()
+        console.log("\033[32m["+username+"] "+message)
+        return
+    }
+    if(message == "off") {
+        Light.turnOff()
+        console.log("\033[31m["+username+"] "+message)
+        return
+    }else {
+        console.log("\033[0m[" + username + "] " + message)
+    }
+})
+
+
+client.once('spawn', () => {
+    console.log("\033[0;32mSuccessfully Connected to the Server !")
+})
+
+
